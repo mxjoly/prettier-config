22247b81474d04afe90d3837dd14f18cdfa1b5c3
Maxime Joly <maxime.joly2808@gmail.com>
2020-10-14 00:03:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Prettier Config
+
+Prettier config shared for all the [@mxjoly](https://github.com/mxjoly) packages published.
+
+## Installation
+
+```bash
+yarn add --dev @mxjoly/prettier-config
+```
+
+```bash
+npm install --save-dev @mxjoly/prettier-config
+```
+
+## Setup
+
+If you want to use this config as base in any of your projects, you need to create a `.prettierrc.js` file in your project folder that re-exports the Prettier config from `@mxjoly/prettier-config`.
+
+You can either do that automatically by running the following command in the root folder of your project (where your `package.json` is):
+
+```bash
+# create .prettierrc.js config file
+npm @mxjoly/prettier-config
+```
+
+or by manually adding the following content:
+
+```js
+module.exports = require('@mxjoly/prettier-config');
+```
+
+**Important:** filename must be `.prettierrc.js` or `prettier.config.js` or otherwise Prettier will try to parse it as JSON or YML and will fail.
+
+Read the Prettier [docs](https://prettier.io/docs/en/configuration.html#sharing-configurations) on sharing configurations for more info.
---
+++ init.js
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const checkForPackageJson = () => {
+  if (fs.existsSync(path.join(process.cwd(), 'package.json')) === false) {
+    console.error(
+      "ERROR: Could not generate Prettier config because package.json does not exist. Run this command from within your project's root folder"
+    );
+    process.exit(1);
+  }
+};
+
+checkForPackageJson();
+
+const configFilePath = path.join(process.cwd(), '.prettierrc.js');
+const configFileExists = fs.existsSync(configFilePath);
+
+if (configFileExists) {
+  console.warn(
+    'WARNING: Prettier config (.prettierrc.js) already exists. Doing nothing.'
+  );
+  process.exit(0);
+}
+
+const alternativeConfigFilenames = [
+  '.prettierrc',
+  '.prettierrc.json',
+  '.prettierrc.yaml',
+  '.prettierrc.yml',
+  '.prettierrc.cjs',
+  'prettier.config.js',
+  '.prettierrc.toml',
+];
+
+const checkAlternativeConfigFilenames = () => {
+  const existingConfigs = alternativeConfigFilenames.reduce((acc, filename) => {
+    if (fs.existsSync(path.join(process.cwd(), filename))) {
+      return acc.concat(filename);
+    }
+    return acc;
+  }, []);
+  return existingConfigs;
+};
+
+const doesPackageJsonConfigExist = () => {
+  const pkg = require(path.join(process.cwd(), 'package.json'));
+  return pkg && pkg.prettier ? true : false;
+};
+
+if (doesPackageJsonConfigExist()) {
+  console.warn(
+    'WARNING: Prettier config found in package.json. Please remove the "prettier" property to avoid conflicts.'
+  );
+}
+
+const alternativeConfigs = checkAlternativeConfigFilenames();
+const alternativeConfigFileExists = alternativeConfigs.length > 0;
+
+if (alternativeConfigFileExists) {
+  console.warn('WARNING: Alternative prettier config file(s) exist:');
+  alternativeConfigs.forEach((file) => {
+    console.warn(`- ${file}`);
+  });
+  console.warn(
+    'Please delete all config files other than .prettierrc.js to avoid conflicts.'
+  );
+}
+
+try {
+  console.log('');
+  console.log('Writing config to', configFilePath);
+  fs.writeFileSync(
+    configFilePath,
+    `module.exports = require('@mxjoly/prettier-config');`
+  );
+  console.log('Prettier config written successfully !');
+  process.exit(0);
+} catch (error) {
+  console.error(
+    'ERROR: Failed to write config file. Please try again or create config manually.'
+  );
+  process.exit(1);
+}
